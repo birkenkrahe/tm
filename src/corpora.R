9ba9133859ad4c0b72e709c57c60795557f69ab2
Marcus Birkenkrahe <birkenkrahe@lyon.edu>
2023-03-31 12:14:28 -0500

    update

--- src/corpora.R
+++ src/corpora.R
@@ -6,12 +6,10 @@ coffee_df <- read.csv("coffee.csv")
 coffee_vec <- coffee_df$text
 coffee_src <- VectorSource(coffee_vec)
 coffee_corpus <- VCorpus(coffee_src)
-content(coffee_corpus[[998]])
 chardonnay_df <- read.csv("chardonnay.csv")
 chardonnay_vec <- chardonnay_df$text
 chardonnay_src <- VectorSource(chardonnay_vec)
 chardonnay_corpus <- VCorpus(chardonnay_src)
-content(chardonnay_corpus[[998]])
 clean_coffee_corpus <- function(corpus) {
     corpus <- tm_map(corpus,
                      removePunctuation)
@@ -32,17 +30,16 @@ clean_chardonnay_corpus <- function(corpus) {
                      content_transformer(tolower))
     corpus <- tm_map(corpus,
                      removeWords,
-                     words = c(stopwords("en"), "chardonnay"))
+                     words = c(stopwords("en"), "chardonnay",
+                               "glass","glasses","wine","amp"))
     corpus <- tm_map(corpus,
                      stripWhitespace)
     return(corpus)
 }
 clean_chardonnay <- clean_chardonnay_corpus(chardonnay_corpus)
 load_packages <- function() {
-    library(tm)
     library(qdap)
     library(SnowballC)
     library(wordcloud)
-    search()
 }
 load_packages()
